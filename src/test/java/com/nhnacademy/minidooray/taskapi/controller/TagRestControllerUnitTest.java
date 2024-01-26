a46000be9d5b2cfbadfa1d6206ba9419075f4b08
minm063 <jangmin063@gmail.com>
2024-01-26 21:19:26 +0900

    feat: tag unit test

--- src/test/java/com/nhnacademy/minidooray/taskapi/controller/TagRestControllerUnitTest.java
+++ src/test/java/com/nhnacademy/minidooray/taskapi/controller/TagRestControllerUnitTest.java
@@ -2,27 +2,31 @@ package com.nhnacademy.minidooray.taskapi.controller;
 
 
 import static org.hamcrest.core.IsEqual.equalTo;
-import static org.mockito.BDDMockito.*;
+import static org.mockito.BDDMockito.any;
+import static org.mockito.BDDMockito.anyLong;
+import static org.mockito.BDDMockito.doNothing;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.nhnacademy.minidooray.taskapi.domain.TagRequest;
 import com.nhnacademy.minidooray.taskapi.domain.TagResponse;
+import com.nhnacademy.minidooray.taskapi.exception.ValidationException;
 import com.nhnacademy.minidooray.taskapi.service.TagService;
 import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
@@ -60,10 +64,9 @@ class TagRestControllerUnitTest {
 
     @Test
     void createTag() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
         TagResponse tagResponse = new TagResponse(1L, 1L, "java");
         given(tagService.createTag(any())).willReturn(tagResponse);
-
-        ObjectMapper objectMapper = new ObjectMapper();
 
         mockMvc.perform(post("/api/tags")
                         .content(objectMapper.writeValueAsString(tagResponse))
@@ -76,10 +79,51 @@ class TagRestControllerUnitTest {
     }
 
     @Test
-    void updateTag() {
+    void updateTag() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        TagResponse tagResponse = new TagResponse(1L, 1L, "java");
+        given(tagService.updateTag(anyLong(), any())).willReturn(tagResponse);
+
+        mockMvc.perform(put("/api/tags/{tagId}", 1)
+                        .content(objectMapper.writeValueAsString(tagResponse))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.tagId", equalTo(1)))
+                .andExpect(jsonPath("$.tagName", equalTo("java")));
     }
 
     @Test
-    void deleteTag() {
+    void deleteTag() throws Exception {
+        doNothing().when(tagService).deleteTag(anyLong());
+
+        mockMvc.perform(delete("/api/tags/{tagId}", 1))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.response", equalTo("OK")));
+    }
+
+    @Test
+    void createTagValidationTest() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        given(tagService.createTag(any())).willThrow(ValidationException.class);
+
+        mockMvc.perform(post("/api/tags")
+                        .content(mapper.writeValueAsString(new TagRequest()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void updateTagValidationTest() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+
+        given(tagService.updateTag(anyLong(), any())).willThrow(ValidationException.class);
+
+        mockMvc.perform(put("/api/tags/{tagId}", 1)
+                        .content(mapper.writeValueAsString(new TagRequest()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest());
     }
 }
