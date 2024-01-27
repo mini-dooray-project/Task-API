8a5393c4e3f54b2b5b0b5769115aff0d62af0819
minm063 <jangmin063@gmail.com>
2024-01-27 19:57:58 +0900

    refactor: exception 대응 추가

--- src/test/java/com/nhnacademy/minidooray/taskapi/controller/MilestoneRestControllerTest.java
+++ src/test/java/com/nhnacademy/minidooray/taskapi/controller/MilestoneRestControllerTest.java
@@ -1,10 +1,12 @@
 package com.nhnacademy.minidooray.taskapi.controller;
 
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -17,6 +19,8 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.nhnacademy.minidooray.taskapi.domain.MilestoneRequest;
 import com.nhnacademy.minidooray.taskapi.domain.MilestoneResponse;
+import com.nhnacademy.minidooray.taskapi.exception.MilestoneNotExistException;
+import com.nhnacademy.minidooray.taskapi.exception.ProjectNotExistException;
 import com.nhnacademy.minidooray.taskapi.exception.ValidationException;
 import com.nhnacademy.minidooray.taskapi.service.MilestoneService;
 import java.time.LocalDateTime;
@@ -62,6 +66,16 @@ class MilestoneRestControllerTest {
     }
 
     @Test
+    void getMilestone_thenThrowMilestoneNotExistException() throws Exception {
+        given(milestoneService.getMilestone(anyLong())).willThrow(MilestoneNotExistException.class);
+
+
+        mockMvc.perform(get("/api/milestones/{milestoneId}", 1))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof MilestoneNotExistException));
+    }
+
+    @Test
     void createMilestone() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.registerModule(new JavaTimeModule());
@@ -70,13 +84,42 @@ class MilestoneRestControllerTest {
                 new MilestoneResponse(1L, 1L, "name", LocalDateTime.now(), LocalDateTime.now()));
 
         mockMvc.perform(post("/api/milestones")
-                        .content(objectMapper.writeValueAsString(new MilestoneRequest(1L, "name", LocalDateTime.now(), LocalDateTime.now())))
+                        .content(objectMapper.writeValueAsString(
+                                new MilestoneRequest(1L, "name", LocalDateTime.now(), LocalDateTime.now())))
                         .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.milestoneId", equalTo(1)))
                 .andExpect(jsonPath("$.projectId", equalTo(1)))
                 .andExpect(jsonPath("$.milestoneName", equalTo("name")));
+    }
+
+    @Test
+    void createMilestone_thenThrowProjectNotExistException() throws Exception {
+        given(milestoneService.createMilestone(any())).willThrow(ProjectNotExistException.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        mockMvc.perform(post("/api/milestones")
+                        .content(objectMapper.writeValueAsString(
+                                new MilestoneRequest(1L, "name", LocalDateTime.now(), LocalDateTime.now())))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ProjectNotExistException));
+    }
+
+    @Test
+    void createMilestone_thenThrowValidationException() throws Exception {
+        given(milestoneService.createMilestone(any())).willThrow(ValidationException.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        mockMvc.perform(post("/api/milestones")
+                        .content(objectMapper.writeValueAsString(
+                                new MilestoneRequest()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ValidationException));
     }
 
     @Test
@@ -88,7 +131,8 @@ class MilestoneRestControllerTest {
                 new MilestoneResponse(1L, 1L, "name", LocalDateTime.now(), LocalDateTime.now()));
 
         mockMvc.perform(put("/api/milestones/{milestoneId}", 1).contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(new MilestoneRequest(1L, "name", LocalDateTime.now(), LocalDateTime.now()))))
+                        .content(objectMapper.writeValueAsString(
+                                new MilestoneRequest(1L, "name", LocalDateTime.now(), LocalDateTime.now()))))
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.milestoneId", equalTo(1)))
@@ -97,30 +141,63 @@ class MilestoneRestControllerTest {
     }
 
     @Test
+    void updateMilestone_thenThrowMilestoneNotExistException() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        given(milestoneService.updateMilestone(anyLong(), any())).willThrow(MilestoneNotExistException.class);
+
+        mockMvc.perform(put("/api/milestones/{milestoneId}", 1).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new MilestoneRequest(1L, "name", LocalDateTime.now(), LocalDateTime.now()))))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof MilestoneNotExistException));
+    }
+
+    @Test
+    void updateMilestone_thenThrowProjectNotExistException() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        given(milestoneService.updateMilestone(anyLong(), any())).willThrow(ProjectNotExistException.class);
+
+        mockMvc.perform(put("/api/milestones/{milestoneId}", 1).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new MilestoneRequest(1L, "name", LocalDateTime.now(), LocalDateTime.now()))))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ProjectNotExistException));
+    }
+
+    @Test
+    void updateMilestone_thenThrowValidationException() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+
+        given(milestoneService.updateMilestone(anyLong(), any())).willThrow(ValidationException.class);
+
+        mockMvc.perform(put("/api/milestones/{milestoneId}", 1).contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(
+                                new MilestoneRequest())))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ValidationException));
+    }
+
+    @Test
     void deleteMilestone() throws Exception {
         doNothing().when(milestoneService).deleteMilestone(anyLong());
 
         mockMvc.perform(delete("/api/milestones/{milestoneId}", 1))
+                .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
                 .andExpect(jsonPath("$.response", equalTo("OK")));
     }
 
     @Test
-    void createMilestoneValidation() throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        given(milestoneService.createMilestone(any())).willThrow(ValidationException.class);
+    void deleteMilestone_thenThrowMilestoneNotExistException() throws Exception {
+        doThrow(MilestoneNotExistException.class).when(milestoneService).deleteMilestone(anyLong());
 
-        mockMvc.perform(post("/api/milestones").content(objectMapper.writeValueAsString(new MilestoneRequest()))
-                .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void updateMilestoneValidation() throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
-        given(milestoneService.updateMilestone(anyLong(), any())).willThrow(ValidationException.class);
-
-        mockMvc.perform(
-                put("/api/milestones/{milestoneId}", 1).content(objectMapper.writeValueAsString(new MilestoneRequest()))
-                        .contentType(MediaType.APPLICATION_JSON)).andExpect(status().isBadRequest());
+        mockMvc.perform(delete("/api/milestones/{milestoneId}", 1))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof MilestoneNotExistException));
     }
 }
