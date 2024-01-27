68c86000055db1708bf951648e9838852ce84235
minm063 <jangmin063@gmail.com>
2024-01-27 20:24:06 +0900

    refactor: exception 조건에 대해 추가

--- src/test/java/com/nhnacademy/minidooray/taskapi/controller/TagRestControllerUnitTest.java
+++ src/test/java/com/nhnacademy/minidooray/taskapi/controller/TagRestControllerUnitTest.java
@@ -2,10 +2,12 @@ package com.nhnacademy.minidooray.taskapi.controller;
 
 
 import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.BDDMockito.any;
 import static org.mockito.BDDMockito.anyLong;
 import static org.mockito.BDDMockito.doNothing;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doThrow;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -17,9 +19,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.nhnacademy.minidooray.taskapi.domain.TagRequest;
 import com.nhnacademy.minidooray.taskapi.domain.TagResponse;
+import com.nhnacademy.minidooray.taskapi.exception.ProjectNotExistException;
+import com.nhnacademy.minidooray.taskapi.exception.TagNotExistException;
 import com.nhnacademy.minidooray.taskapi.exception.ValidationException;
 import com.nhnacademy.minidooray.taskapi.service.TagService;
 import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -63,6 +68,16 @@ class TagRestControllerUnitTest {
     }
 
     @Test
+    void getTag_thenThrowTagNotExistException() throws Exception {
+
+        given(tagService.getTag(anyLong())).willThrow(TagNotExistException.class);
+
+        mockMvc.perform(get("/api/tags/{tagId}", 1))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof TagNotExistException));
+    }
+
+    @Test
     void createTag() throws Exception {
         ObjectMapper objectMapper = new ObjectMapper();
         TagResponse tagResponse = new TagResponse(1L, 1L, "java");
@@ -76,6 +91,29 @@ class TagRestControllerUnitTest {
                 .andExpect(jsonPath("$.tagId", equalTo(1)))
                 .andExpect(jsonPath("$.tagName", equalTo("java")));
 
+    }
+    @Test
+    void createTagValidationTest() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        given(tagService.createTag(any())).willThrow(ValidationException.class);
+
+        mockMvc.perform(post("/api/tags")
+                        .content(mapper.writeValueAsString(new TagRequest()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ValidationException));
+    }
+
+    @Test
+    void createTag_thenThrowTagNotExistException() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        given(tagService.createTag(any())).willThrow(TagNotExistException.class);
+
+        mockMvc.perform(post("/api/tags")
+                        .content(objectMapper.writeValueAsString(new TagRequest(1L, "test")))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof TagNotExistException));
     }
 
     @Test
@@ -94,6 +132,42 @@ class TagRestControllerUnitTest {
     }
 
     @Test
+    void updateTagValidationTest() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        given(tagService.updateTag(anyLong(), any())).willThrow(ValidationException.class);
+
+        mockMvc.perform(put("/api/tags/{tagId}", 1)
+                        .content(mapper.writeValueAsString(new TagRequest()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ValidationException));
+    }
+
+    @Test
+    void updateTag_thenThrowProjectNotExistException() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        given(tagService.updateTag(anyLong(), any())).willThrow(ProjectNotExistException.class);
+
+        mockMvc.perform(put("/api/tags/{tagId}", 1)
+                        .content(mapper.writeValueAsString(new TagRequest(1L, "name")))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof ProjectNotExistException));
+    }
+
+    @Test
+    void updateTag_thenThrowTagNotExistException() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        given(tagService.updateTag(anyLong(), any())).willThrow(TagNotExistException.class);
+
+        mockMvc.perform(put("/api/tags/{tagId}", 1)
+                        .content(mapper.writeValueAsString(new TagRequest(1L, "name")))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof TagNotExistException));
+    }
+
+    @Test
     void deleteTag() throws Exception {
         doNothing().when(tagService).deleteTag(anyLong());
 
@@ -104,24 +178,11 @@ class TagRestControllerUnitTest {
     }
 
     @Test
-    void createTagValidationTest() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        given(tagService.createTag(any())).willThrow(ValidationException.class);
+    void deleteTag_thenThrowTagNotExistException() throws Exception {
+        doThrow(TagNotExistException.class).when(tagService).deleteTag(anyLong());
 
-        mockMvc.perform(post("/api/tags")
-                        .content(mapper.writeValueAsString(new TagRequest()))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
-    }
-
-    @Test
-    void updateTagValidationTest() throws Exception {
-        ObjectMapper mapper = new ObjectMapper();
-        given(tagService.updateTag(anyLong(), any())).willThrow(ValidationException.class);
-
-        mockMvc.perform(put("/api/tags/{tagId}", 1)
-                        .content(mapper.writeValueAsString(new TagRequest()))
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isBadRequest());
+        mockMvc.perform(delete("/api/tags/{tagId}", 1))
+                .andExpect(status().isBadRequest())
+                .andExpect(result -> assertTrue(result.getResolvedException() instanceof TagNotExistException));
     }
 }
