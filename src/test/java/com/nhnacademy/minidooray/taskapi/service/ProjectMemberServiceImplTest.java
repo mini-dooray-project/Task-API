bef39046b895097b42ef61cd7d030873a046cb3e
minm063 <jangmin063@gmail.com>
2024-01-28 02:02:49 +0900

    feat: projectMemberServiceTest

--- src/test/java/com/nhnacademy/minidooray/taskapi/service/ProjectMemberServiceImplTest.java
+++ src/test/java/com/nhnacademy/minidooray/taskapi/service/ProjectMemberServiceImplTest.java
@@ -1,24 +1,34 @@
 package com.nhnacademy.minidooray.taskapi.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import com.nhnacademy.minidooray.taskapi.domain.ProjectMemberModifyRequest;
 import com.nhnacademy.minidooray.taskapi.domain.ProjectMemberRegisterRequest;
 import com.nhnacademy.minidooray.taskapi.domain.ProjectMemberResponse;
+import com.nhnacademy.minidooray.taskapi.entity.Project;
+import com.nhnacademy.minidooray.taskapi.entity.ProjectAuthority;
+import com.nhnacademy.minidooray.taskapi.entity.ProjectMember;
 import com.nhnacademy.minidooray.taskapi.exception.MemberNotExistException;
 import com.nhnacademy.minidooray.taskapi.exception.ProjectNotExistException;
+import com.nhnacademy.minidooray.taskapi.repository.ProjectAuthorityRepository;
 import com.nhnacademy.minidooray.taskapi.repository.ProjectMemberRepository;
+import com.nhnacademy.minidooray.taskapi.repository.ProjectRepository;
 import java.util.List;
-import org.assertj.core.api.Assertions;
+import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 
 @ExtendWith(MockitoExtension.class)
 class ProjectMemberServiceImplTest {
@@ -29,10 +39,17 @@ class ProjectMemberServiceImplTest {
     @Mock
     private ProjectMemberRepository projectMemberRepository;
 
+    @Mock
+    private ProjectRepository projectRepository;
+
+    @Mock
+    private ProjectAuthorityRepository authorityRepository;
+
     @Test
     void getMembersByProject() {
         // given
         Long projectId = 10L;
+        ProjectMemberResponse memberResponse = createMemberResponse();
 
         // mocking
         given(projectMemberRepository.existsByPk_projectId(anyLong()))
@@ -44,13 +61,14 @@ class ProjectMemberServiceImplTest {
         List<ProjectMemberResponse> responses = projectMemberService.getMembersByProject(projectId);
 
         // then
-        List<ProjectMemberResponse> byPkProjectId = projectMemberRepository.findByPk_projectId(projectId);
+        assertNotNull(responses);
+        assertEquals(responses.get(0).getMemberId(), memberResponse.getMemberId());
+        assertEquals(responses.get(0).getProjectId(), memberResponse.getProjectId());
+        assertEquals(responses.get(0).getProjectName(), memberResponse.getProjectName());
+        assertEquals(responses.get(0).getAuthorityId(), memberResponse.getAuthorityId());
+        assertEquals(responses.get(0).getAuthorityName(), memberResponse.getAuthorityName());
 
-        assertEquals(responses.get(0).getMemberId(), byPkProjectId.get(0).getMemberId());
-        assertEquals(responses.get(0).getProjectId(), byPkProjectId.get(0).getProjectId());
-        assertEquals(responses.get(0).getProjectName(), byPkProjectId.get(0).getProjectName());
-        assertEquals(responses.get(0).getAuthorityId(), byPkProjectId.get(0).getAuthorityId());
-        assertEquals(responses.get(0).getAuthorityName(), byPkProjectId.get(0).getAuthorityName());
+        verify(projectMemberRepository, times(1)).findByPk_projectId(projectId);
     }
 
     @Test
@@ -63,6 +81,7 @@ class ProjectMemberServiceImplTest {
 
         assertThrows(ProjectNotExistException.class, () -> projectMemberService.getMembersByProject(projectId));
     }
+
     @Test
     void getMemberByMemberId() {
         String memberId = "user";
@@ -94,11 +113,46 @@ class ProjectMemberServiceImplTest {
     @Test
     void createMember() {
         ProjectMemberRegisterRequest memberRegisterRequest = new ProjectMemberRegisterRequest("user", 1L, 1L);
-//        ReflectionTestUtils.setField(memberRegisterRequest, "memberRequest", );
+
+        given(projectRepository.findById(anyLong())).willReturn(Optional.of(new Project()));
+        given(authorityRepository.findById(anyLong())).willReturn(Optional.of(new ProjectAuthority()));
+
+        ProjectMemberResponse memberResponse = projectMemberService.createMember(memberRegisterRequest);
+
+        assertNotNull(memberResponse);
+        verify(projectMemberRepository, times(1)).save(any(ProjectMember.class));
     }
 
     @Test
     void updateMember() {
+        String memberId = "user";
+        Long projectId = 1L;
+        ProjectMemberModifyRequest modifyRequest = new ProjectMemberModifyRequest(1L);
+
+        given(projectRepository.existsById(projectId)).willReturn(true);
+        when(authorityRepository.findById(modifyRequest.getAuthorityId())).thenReturn(
+                Optional.of(new ProjectAuthority(1L, "name")));
+        when(projectMemberRepository.findById(any())).thenReturn(
+                Optional.of(new ProjectMember(new ProjectMember.Pk(memberId, projectId), new Project(),
+                        new ProjectAuthority())));
+
+        ProjectMemberResponse response = projectMemberService.updateMember(memberId, projectId, modifyRequest);
+
+        assertNotNull(response);
+        verify(projectRepository, times(1)).existsById(anyLong());
+        verify(authorityRepository, times(1)).findById(anyLong());
+        verify(projectMemberRepository, times(1)).findById(any());
+    }
+
+    @Test
+    void updateMember_thenThrowProjectNotExistException() throws Exception {
+        String memberId = "user";
+        Long projectId = 1L;
+        ProjectMemberModifyRequest modifyRequest = new ProjectMemberModifyRequest();
+
+        when(projectRepository.existsById(anyLong())).thenReturn(false);
+        assertThrows(ProjectNotExistException.class,
+                () -> projectMemberService.updateMember(memberId, projectId, modifyRequest));
     }
 
     private ProjectMemberResponse createMemberResponse() {
