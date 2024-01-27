e0f3bfd27cb6bd216cf0f6bdefa9ee347b1ca708
minm063 <jangmin063@gmail.com>
2024-01-28 02:02:23 +0900

    feat: project service test

---
+++ src/test/java/com/nhnacademy/minidooray/taskapi/service/ProjectServiceImplTest.java
@@ -0,0 +1,46 @@
+package com.nhnacademy.minidooray.taskapi.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.nhnacademy.minidooray.taskapi.domain.ProjectResponse;
+import com.nhnacademy.minidooray.taskapi.exception.ProjectNotExistException;
+import com.nhnacademy.minidooray.taskapi.repository.ProjectRepository;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ProjectServiceImplTest {
+
+    @InjectMocks
+    private ProjectServiceImpl projectService;
+
+    @Mock
+    private ProjectRepository projectRepository;
+
+
+    @Test
+    void getProjects() {
+        when(projectRepository.findAllBy()).thenReturn(List.of(new ProjectResponse()));
+
+        List<ProjectResponse> responses = projectService.getProjects();
+
+        verify(projectRepository, times(1)).findAllBy();
+        assertNotNull(responses);
+    }
+
+    @Test
+    void getProject() {
+        when(projectRepository.existsById(anyLong())).thenReturn(false);
+
+        assertThrows(ProjectNotExistException.class, () -> projectService.getProject(anyLong()));
+        verify(projectRepository, times(1)).existsById(anyLong());
+    }
+}
